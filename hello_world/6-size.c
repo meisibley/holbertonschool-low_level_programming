bf3a6c88026352ddd4e0f381edb7ce015faba8a6
meisibley <mkong99@yahoo.com>
2023-05-24 15:10:35 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+/**
+ * main - prints the size of various types
+ *
+ * Return - always be 0
+ */
+#include <stdio.c>
+int main(void)
+{
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	char c;
+
+	printf("Size of a char: %lu. \n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu. \n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu. \n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu. \n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu. \n", (unsigned long)sizeof(f));
+
+	return (0);
+}

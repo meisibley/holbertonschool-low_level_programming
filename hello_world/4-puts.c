b4af30a7e4859a710bb4fec66cbe8a1d24f0e515
meisibley <mkong99@yahoo.com>
2023-05-24 14:05:43 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * function_name - puts
+ * main - puts
  * @parameterx: print a string
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

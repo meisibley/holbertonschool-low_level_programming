c6778d91661da3a00489031b78b10bdcbb67c79c
meisibley <mkong99@yahoo.com>
2023-05-24 13:33:25 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts();
+	return (0);
+}
+
+void puts(void)
+{
+	putchar("Programming is like building a multilingual puzzle.\n");
+}

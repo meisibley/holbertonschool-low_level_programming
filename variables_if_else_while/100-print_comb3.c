63e65b06c8a34f5a0702a064f65b01d0f40f5755
meisibley <mkong99@yahoo.com>
2023-05-25 13:40:39 -0700

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two different digis, w/o repeat
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	for (j = 0; j <= 9; ++j)
+		if (i != j && i < j)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}

e0a467aa8cd271b990ee6c0c8e31255eb6455359
meisibley <mkong99@yahoo.com>
2023-05-25 08:34:18 -0700

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - pick a random number and check it's positive, zero, or negative
+ *
+ * Return: zero if function is success 
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if ( n > 0 )
+		printf("%d is positive\n", n);
+	else if ( n < 0 )
+		printf("%d is negative\n", n);
+	else 
+		printf("%d is zero\n", n);
+
+	return (0);
+}

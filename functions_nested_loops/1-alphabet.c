63d944275d90a8dc49f7aff025b719f47f7a2f77
meisibley <mkong99@yahoo.com>
2023-05-26 14:37:40 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, using _putchar only
+ * print_alphabet - prints the alphabet, using _putchar only
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
 
 void print_alphabet(void)

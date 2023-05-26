26631401e990e5109439e8470b3f2acdd770bf10
meisibley <mkong99@yahoo.com>
2023-05-26 15:23:08 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: no return
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	print_alphabet();
+}

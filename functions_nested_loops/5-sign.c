e0ae72d4593a533aaf38af5475fa416c5d65080d
meisibley <mkong99@yahoo.com>
2023-05-27 23:34:55 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - check if input is a positive, negtive or zero number
+ * @n: number for check
+ * Return: positive returns 1 and prints +; negtive returns -1 and prints -; zero 0 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

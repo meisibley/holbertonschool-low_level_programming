fb6afb84c39b4ff843c4fb3ddae02373992bd581
meisibley <mkong99@yahoo.com>
2023-05-28 14:57:32 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ *
+ * Return: the added result
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - adds two integers and returns the result
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}

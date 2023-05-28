9104c2eddac49b73d902cc120f3db18fd7805dd1
meisibley <mkong99@yahoo.com>
2023-05-27 22:03:40 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ * @c: character for checking
+ * Returns: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' )
+		return (1);
+	else
+		return (0);
+}
---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - call function _islower to checks for lowercase character.
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}

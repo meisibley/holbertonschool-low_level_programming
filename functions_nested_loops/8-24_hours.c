dc231ca087ed50f2085e4724bec716fd2bed8cb9
meisibley <mkong99@yahoo.com>
2023-05-28 13:18:27 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ * @h1: first digit of hour number
+ * @h2: second digit of hour number
+ * @m1: first digit of minute number
+ * @m2: second digit of minute number
+ * Return: from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	for (h2 = 0; h2 <= 3; h2++)
+	for (m1 = 0; m1 <= 5; m1++)
+	for (m2 = 0; m2 <= 9; m2++)
+	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+	}
+}
---
+++ functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print every minute of the day of Jack Bauer.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}

681f8122dc5faec5c545bc5ea6d9810bb9b7f803
meisibley <mkong99@yahoo.com>
2023-05-30 12:12:56 -0700

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,41 +9,30 @@ int main(void)
 	int i;
 	unsigned long int j, k, f, j1, j2, k1, k2, f1, f2;
 
-	j = 1;
-	f = 2;
+	j = 1, f = 2;
 	printf("%lu, %lu, ", j, f);
 	for (i = 1; i <= 90; i++)
 	{
-		k = j;
-		j = f;
-		f = k + f;
+		k = j, j = f, f = k + f;
 		printf("%lu, ", f);
 	}
 	for (i = 91; i <= 96; i++)
 	{
 		if (i == 91)
 		{
-			k1 = j / 10000;
-			k2 = j % 10000;
-			j1 = f / 10000;
-			j2 = f % 10000;
+			k1 = j / 10000, k2 = j % 10000, j1 = f / 10000, j2 = f % 10000;
 		}
 		else
 		{
-			k1 = j1;
-			k2 = j2;
-			j1 = f1;
-			j2 = f2;
+			k1 = j1, k2 = j2, j1 = f1, j2 = f2;
 		}
 		if ((k2 + j2) > 9999)
 		{
-			f1 = k1 + j1 + 1;
-			f2 = (k2 + j2) % 10000;
+			f1 = k1 + j1 + 1, f2 = (k2 + j2) % 10000;
 		}
 		else
 		{
-			f1 = k1 + j1;
-			f2 = k2 + j2;
+			f1 = k1 + j1, f2 = k2 + j2;
 		}
 		if (i != 96)
 			printf("%lu%lu, ", f1, f2);

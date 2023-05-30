2a7f8d240d471854e06a48c71009b0a3102a68fa
meisibley <mkong99@yahoo.com>
2023-05-29 20:55:11 -0700

    102-print_combs.c

---
+++ variables_if_else_while/102-print_combs.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints all possible diff comb of two two-digis numbers
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i, j, m, n, s1, s2;
+
+	for (i = 0; i <= 9; i++)
+	for (j = 0; j <= 8; j++)
+	for (m = i; m <= 9; m++)
+	for (n = j + 1; n <= 9; n++)
+	{
+		s1 = i * 10 + j;
+		s2 = m * 10 + n;
+		if (s1 < s2)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			putchar(' ');
+			putchar(48 + m);
+			putchar(48 + n);
+			if (s1 != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

c78af47d2b9b354528cb1d610c3900d41206618c
meisibley <mkong99@yahoo.com>
2023-05-29 19:36:47 -0700

    101-print_comb4.c

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible diff comb of three diff digis, w/o repeat
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+	for (j = i - 1; j <= 8; j++)
+	{
+	for (k = j - 1; k <= 9; k++)
+	{
+		if (k > j && j > i)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			putchar(48 + k);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

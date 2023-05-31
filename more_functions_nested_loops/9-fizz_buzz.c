3158a019d6df8943b6da808c87ae8868badbb831
meisibley <mkong99@yahoo.com>
2023-05-31 13:48:00 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
+		if (i == 100)
+			printf("Buzz");
+		else if ((i % 3) == 0 && (i % 5) != 0)
 			printf("Fizz ");
 		else if ((i % 3) != 0 && (i % 5) == 0)
 			printf("Buzz ");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz ");
-		else if (i == 100)
-			printf("%d", i);
-		else if (i != 100)
+		else
 			printf("%d ", i);
 	}
 	printf("\n");

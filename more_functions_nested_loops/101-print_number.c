2886c94f8d47c8ebb2332f0ea45c1ac2868a886a
meisibley <mkong99@yahoo.com>
2023-06-01 00:17:40 -0700

    101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,10 @@
 
 /**
  * print_number - a function prints an integer
- * print_posi_num - only prints positive integer
  * @n: input number
  * Return: no return
  */
-void print_number(long int n)
+void print_number(int n)
 {
 	int i, j, j1, n1, n2;
 	int flag = 0;
@@ -17,13 +16,11 @@ void print_number(long int n)
 	{
 		n = -n;
 		_putchar('-');
-
 		for (i = 1000000000; i > 0; i = i / 10)
 		{
 			j = n / i;
 			if (j != 0)
 				flag = 1;
-
 			if (j != 0 || flag == 1)
 				_putchar(j + '0');
 			n = n - (n / i) * i;
@@ -35,23 +32,19 @@ void print_number(long int n)
 			j = n / i;
 			if (j != 0)
 				flag = 1;
-
 			if (j != 0 || flag == 1)
 				_putchar(j + '0');
 			n = n - (n / i) * i;
 		}
 	else if (n < -2147483647)
 	{
-		n1 = -n / 10;
-		n2 = -n % 10;
+		n1 = -n, n1 = n1 / 10, n2 = n1 % 10;
 		_putchar('-');
-
 		for (i = 100000000; i > 0; i = i / 10)
 		{
 			j1 = n1 / i;
 			if (j1 != 0)
 				flag = 1;
-
 			if (j1 != 0 || flag == 1)
 				_putchar(j1 + '0');
 			n1 = n1 - (n1 / i) * i;
@@ -60,14 +53,12 @@ void print_number(long int n)
 	}
 	else if (n > 2147483647)
 	{
-		n1 = -n / 10;
-		n2 = -n % 10;
+		n1 = n / 10, n2 = n % 10;
 		for (i = 1000000000; i > 0; i = i / 10)
 		{
 			j1 = n1 / i;
 			if (j1 != 0)
 				flag = 1;
-
 			if (j1 != 0 || flag == 1)
 				_putchar(j1 + '0');
 			n1 = n1 - (n1 / i) * i;
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -13,6 +13,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(long int n);
+void print_number(int n);
 
 #endif

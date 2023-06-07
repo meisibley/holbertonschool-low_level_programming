37824e9e2a3faf16e8b4cb1c9fad08314823ece8
meisibley <mkong99@yahoo.com>
2023-06-07 10:05:04 -0700

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concat two strings, 2nd string only include n digits
+ * @dest: the first input string
+ * @src: the second input string
+ * @n: the digits of 2nd string should be concated
+ * Return: concated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j = 0;
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}

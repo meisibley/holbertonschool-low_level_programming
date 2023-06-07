4f4630ccff1a8a1bacbb73d0ef251358f02552da
meisibley <mkong99@yahoo.com>
2023-06-07 15:51:07 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input string
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i = 0, flag = 0;
+
+	if (i == 0 && str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] = str[0] - 32;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+			flag = 1;
+		else
+			flag = 0;
+		i++;
+		if (flag == 1 && str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	str[i++] = '\0';
+	return (str);
+}

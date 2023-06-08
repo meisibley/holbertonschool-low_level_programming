c7e68c57220d03ef7662da506950cdef430d31cc
meisibley <mkong99@yahoo.com>
2023-06-08 07:18:36 -0700

    101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,31 +7,35 @@
 void print_number(int n)
 {
 	int i, flag = 0;
-	int intmax[] = {2, 1, 4, 7, 4, 8, 3, 6, 4, 8};
 
 	if (n == 0)
 		_putchar(0 + '0');
 	else if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar('-'), n = -n;
 	}
 	if (n <= 2147483647)
 	{
 		for (i = 1000000000; i > 0; i = i / 10)
 		{
 			if ((n / i) > 0)
-			{
 				flag = 1;
-			}
 			if (flag != 0)
-			{
 				_putchar((n / i) + '0');
-			}
 			n = n - (n / i) * i;
 		}
 	}
 	else
-		for (i = 0; i < 10; i++)
-			_putchar(intmax[i] + '0');
+	{
+		_putchar(2 + '0');
+		_putchar(1 + '0');
+		_putchar(4 + '0');
+		_putchar(7 + '0');
+		_putchar(4 + '0');
+		_putchar(8 + '0');
+		_putchar(3 + '0');
+		_putchar(6 + '0');
+		_putchar(4 + '0');
+		_putchar(8 + '0');
+	}
 }

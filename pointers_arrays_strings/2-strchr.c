bf418558406a063fe28a449e0556dbbddf823758
meisibley <mkong99@yahoo.com>
2023-06-08 15:23:13 -0700

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] <= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
-		}
-		else
-		{
-			i++;
 		}
 	}
 	return (NULL);

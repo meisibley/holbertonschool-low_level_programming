b58248c2b3ee7951708651112d0c8c890d6fef6b
meisibley <mkong99@yahoo.com>
2023-06-08 22:59:13 -0700

    5-strstr.c

---
+++ pointers_arrays_strings/25-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - locates a substring
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}

2bd35de9bcabb6d7a680c24f306c0e642c651326
meisibley <mkong99@yahoo.com>
2023-06-10 08:40:56 -0700

    103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -9,50 +9,69 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j1 = 0, j2 = 0, max, carry = 0;
+	int i, j1 = 0, j2 = 0, carry = 0;
+
 	for (i = 0; n1[i] != '\0'; i++)
 		j1++;
 	for (i = 0; n2[i] != '\0'; i++)
 		j2++;
-	if (j1 < j2)
-		max = j2;
-	else
-		max = j1;
-for (i = max - 1; i >= 0; i--)
+if (j1 > j2)
 {
-	if ((n1[i] < 48 || n1[i] > 57) && (n2[i] < 48 || n2[i] > 57))
-	{
-		break;
-	}
+for (i = j1 - 1; i >= j1 - j2; i--)
+{
 	if (carry > 0)
 	{
 		r[i] = 49, carry = 0;
 	}
 	else
 		r[i] = 48;
-	if ((n1[i] < 48 || n1[i] > 57) && (n2[i] >= 48 && n2[i] <= 57))
-		r[i] += n2[i] - 48;
-	else if ((n1[i] >= 48 && n1[i] <= 57) && (n2[i] < 48 || n2[i] > 57))
-		r[i] += n1[i] - 48;
-	else
+	r[i] += n1[i] + n2[i - j1 + j2] - 96;
+	if (r[i] > 57)
 	{
-		r[i] += n1[i] + n2[i] - 96;
-		if (r[i] > 57)
-		{
-			carry = 1, r[i] = r[i] - 10;
-		}
+		carry = 1, r[i] = r[i] - 10;
+	}
+}
+for (i = j1 - j2 - 1; i >= 0; i--)
+{
+	if (carry > 0)
+	{
+		r[i] = 49, carry = 0;
+	}
+	else
+		r[i] = 48;
+	r[i] += n1[i] - 48;
+	if (r[i] > 57)
+	{
+		carry = 1, r[i] = r[i] - 10;
+	}
+}
+}
+else
+for (i = j1 - 1; i >= 0; i--)
+{
+	if (carry > 0)
+	{
+		r[i] = 49, carry = 0;
+	}
+	else
+		r[i] = 48;
+	r[i] += n1[i] + n2[i] - 96;
+	if (r[i] > 57)
+	{
+		carry = 1, r[i] = r[i] - 10;
 	}
 }
 	if (carry > 0)
 	{
-		r[max + 1] = '\0';
-		for (i = max; i > 0; i--)
+		r[j1 + 1] = '\0';
+		for (i = j1; i > 0; i--)
 			r[i] = r[i - 1];
 		r[0] = 49;
+		j1++;
 	}
 	else
-		r[max] = '\0';
-	if (j1 > size_r || j2 > size_r)
+		r[j1] = '\0';
+	if (j1 >= size_r || j2 >= size_r)
 		r = 0;
 	return (r);
 }

641bf6912e0a8be344341b3e9ca086190d40affc
meisibley <mkong99@yahoo.com>
2023-06-14 11:43:51 -0700

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root
+ * @n: a given number
+ * Return: the square root of the number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	if (n / 4 == 0)
+	{
+		n = n / 4;
+		return (2 * _sqrt_recursion(n / 4));
+	}
+	else if (n / 9 == 0)
+	{
+		n = n / 9;
+		return (3 * _sqrt_recursion(n / 9));
+	}
+	else if (n / 25 == 0)
+	{
+		n = n / 25;
+		return (5 * _sqrt_recursion(n / 25));
+	}
+	else if (n / 49 == 0)
+	{
+		n = n / 49;
+		return (7 * _sqrt_recursion(n / 49));
+	}
+	else if (n / 121)
+	{
+		n = n / 121;
+		return (11 * _sqrt_recursion(n / 121));
+	}
+	else if (n % 4 != 0 || n % 9 != 0 || n % 25 != 0 || n % 49 ! = 0)
+		return (-1);
+}

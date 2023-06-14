882b55148e529c409e1b2c23cc5b911cde4b4cf3
meisibley <mkong99@yahoo.com>
2023-06-14 12:35:49 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,37 +7,22 @@
  */
 int _sqrt_recursion(int n)
 {
+	return calsqrt(n, 0);
+}
+
+/**
+ * calsqrt - use recursion to calculate square root
+ * @n: origin number
+ * @r: result
+ * Return: result
+ */
+int calsqrt(int n, int r)
+{
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	if (n % 4 != 0 || n % 9 != 0 || n % 25 != 0 || n % 49 != 0)
-		return (-1);
-	else if (n / 4 == 0)
-	{
-		n = n / 4;
-		return (2 * _sqrt_recursion(n));
-	}
-	else if (n / 9 == 0)
-	{
-		n = n / 9;
-		return (3 * _sqrt_recursion(n));
-	}
-	else if (n / 25 == 0)
-	{
-		n = n / 25;
-		return (5 * _sqrt_recursion(n));
-	}
-	else if (n / 49 == 0)
-	{
-		n = n / 49;
-		return (7 * _sqrt_recursion(n));
-	}
-	else if (n / 121)
-	{
-		n = n / 121;
-		return (11 * _sqrt_recursion(n));
-	}
-	else
-	{}
+	if (r * r == n)
+		return (r);
+	return (calsqrt(n, r + 1);
 }

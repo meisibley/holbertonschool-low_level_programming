0586d45191481fd0bacedff039bc67b2c52324e0
meisibley <mkong99@yahoo.com>
2023-06-15 10:32:50 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
@@ -17,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		sum = (int)argv[1] * (int)argv[2];
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 		return (0);
 	}

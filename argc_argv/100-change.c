28f4a370643376a3b2515dbc46c81b95f2e7153b
meisibley <mkong99@yahoo.com>
2023-06-15 14:37:48 -0700

    100-change.c

---
+++ argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum # of coins to make change for amount of money
+ * @argc: 2
+ * @argv: 2 only, first is excuable file, second is amount of money
+ * Return: 0 negative amount of money, 1 wrong argc number, ttlc
+ */
+int main (int argc, char *argv[])
+{
+	int i, amt = 0, ttlc = 0;
+	int coin[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) <= 0)
+		printf("0\n");
+	else
+	{
+		amt = atoi(argv[1]);
+		for (i = 0; i < 4; i++)
+		{
+			if (amt >= coin[i])
+			{
+				ttlc += amt / coin[i];
+				amt = amt % coin[i];
+			}
+		}
+		ttlc += amt;
+	}
+	printf("%d\n", ttlc);
+	return (ttlc);
+}

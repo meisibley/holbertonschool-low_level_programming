b8698bc85c6a030b17d82567e5e8af32787e4aec
meisibley <mkong99@yahoo.com>
2023-06-15 10:12:17 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: length of argv[]
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int 1;
+
+	if (argc >= 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	return (0);
+}

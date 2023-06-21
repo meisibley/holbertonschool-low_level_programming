dba65c244014476ddf090d3feba053fbc19b9134
meisibley <mkong99@yahoo.com>
2023-06-21 14:19:39 -0700

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	g = malloc(sizeof(int*) * (width * height));
+	g = malloc(sizeof(int*) * width);
 	if (g == NULL)
 	{
 		free(g);

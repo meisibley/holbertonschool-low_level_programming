5a73510228e9b0f927dcf0303ed41c91a40b8acc
meisibley <mkong99@yahoo.com>
2023-06-22 12:51:18 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,13 +8,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	int *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * sizeof(int));
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = '\0';
 	return (p);
 }

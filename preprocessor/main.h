416974e74babe1c53e09aaefdd0790abe92b3b1f
meisibley <mkong99@yahoo.com>
2023-06-23 09:31:23 -0700

    0-object_like_macro.h

---
+++ preprocessor/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int calsqrt(int n, int r);
+int is_prime_number(int n);
+int pri_num(int n, int i);
+int is_palindrome(char *s);
+int cChar(char *s, int len, int j);
+int wildcmp(char *s1, char *s2);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif

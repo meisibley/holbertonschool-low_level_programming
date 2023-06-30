e9d4a338f7c2d4cdba612f1dc193fe0f2a4659de
meisibley <mkong99@yahoo.com>
2023-06-29 19:37:33 -0700

    3-calc_5_ops

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -45,7 +45,7 @@ int op_div(int a, int b)
 	{
 		printf("Error\n");
 		exit(100);
-	}	
+	}
 	return (a / b);
 }
 
@@ -61,6 +61,6 @@ int op_mod(int a, int b)
 	{
 		printf("Error\n");
 		exit(100);
-	}	
+	}
 	return (a % b);
 }

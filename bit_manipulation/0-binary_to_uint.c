12bd6ff01477ccb2b07de88cca9c33905fdf7b89
meisibley <mkong99@yahoo.com>
2023-07-13 12:49:42 -0700

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,45 +8,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	long int result, two = 1;
-	int i, j, k, len, flag = 0;
-	char *bb;
+	long int result = 0, bit;
+	int i, j = 0;
 
 	if (b == NULL)
 		return (0);
-	result = 0;
-	bb = malloc(sizeof(b + 1));
-	{
-		if (bb == NULL)
-			return (0);
-	}
-	for (i = 0, j = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == '1' && flag == 0)
-			flag++;
-		if (flag > 0)
-		{
-			bb[j] = b[i];
-			j++;
-		}
 	}
-	len = j;
-	if (bb[len - 1] == '1')
-		result = 1;
-	else
-		result = 0;
-	for (i = len - 2, j = 1; i >= 0; i--, j++)
+	i--;
+	for (; i >= 0; i--, j++)
 	{
-		if (bb[i] == '1')
-		{
-			for (k = 1; k <= j; k++)
-				two *= 2;
-			result += two;
-		}
-		two = 1;
+		if (b[i] == '0')
+			bit = 0;
+		else
+			bit = 1;
+		result = result + (bit << j);
 	}
-	free(bb);
 	return (result);
 }

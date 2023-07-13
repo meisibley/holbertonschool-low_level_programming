17676a6f44f4f1391f9f0a399dffcf25613d4b45
meisibley <mkong99@yahoo.com>
2023-07-12 17:37:01 -0700

    2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: a pointer points list
+ * @str: the string data will add to list.
+ *
+ * Return: A list_t type list.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}

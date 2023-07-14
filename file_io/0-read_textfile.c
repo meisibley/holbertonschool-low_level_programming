d67dcc5980d50a2a9391193da0ab235fa734735c
meisibley <mkong99@yahoo.com>
2023-07-14 14:40:35 -0700

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: the text file
+ * @letters: count of letters should read and print
+ *
+ * Return: count of letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	size_t n, m, fd;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (!(fd))
+	{
+		free(buf);
+		return (0);
+	}
+	n = read(fd, buf, letters);
+	if (!(n))
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	m = write(STDOUT_FILENO, buf, n);
+	if (!(m))
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (n);
+}

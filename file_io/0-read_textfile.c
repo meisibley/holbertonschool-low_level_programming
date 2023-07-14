b93052dfe50fb2295849f6dae93f64038e823c6e
meisibley <mkong99@yahoo.com>
2023-07-14 14:43:33 -0700

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	size_t n, m, fd;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 	buf = malloc(sizeof(char) * letters + 1);
 	if (buf == NULL)
 	{

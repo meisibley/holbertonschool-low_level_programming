a5d72f0557de9e940d46192b0dd115404ea19a6d
meisibley <mkong99@yahoo.com>
2023-07-14 18:12:53 -0700

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, w, count;
 
 	count = strlen(text_content);
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
 	if (fd == -1)

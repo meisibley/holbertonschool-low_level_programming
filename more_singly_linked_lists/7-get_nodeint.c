4af8b92528c8d7befbe5ac2728bcad7229dd2a5a
meisibley <mkong99@yahoo.com>
2023-07-15 22:10:40 -0700

    7-get_nodeint.c

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return the index node of a listint_t linked list
+ * @head: the pointer points listint_t list
+ * @index: the number of node
+ *
+ * Return: The nth node of a listint_t linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *node, *listnode;
+
+	if (head == NULL)
+		return (NULL);
+	listnode = malloc(sizeof(listint_t));
+	if (listnode == NULL)
+	{
+		free(listnode);
+		return (NULL);
+	}
+	listnode = head;
+	while (listnode != NULL)
+	{
+		if (i == index)
+		{
+			node = listnode;
+			free(listnode);
+			return (node);
+		}
+		i++;
+		listnode = listnode->next;
+	}
+	free(listnode);
+	return (NULL);
+}

83d6f80bdc3d6210f89be4957968c0c50428fc78
meisibley <mkong99@yahoo.com>
2023-07-15 18:08:09 -0700

    3-add_nodeint_end.c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	}
 	temp->n = n;
-	temp->next = NULL;	
+	temp->next = NULL;
 	if (*head == NULL)
 		*head = temp;
 	else

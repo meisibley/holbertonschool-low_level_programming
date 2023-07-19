d3f5806ad18caf337e04533c45c19957130c529f
meisibley <mkong99@yahoo.com>
2023-07-19 15:37:58 -0700

    4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: given dlistint_t list
+ *
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = head;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	return;
+}

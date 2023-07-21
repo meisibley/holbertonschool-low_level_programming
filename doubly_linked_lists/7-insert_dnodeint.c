96f836fe4f5289dbfdfcb4d110d79f9e46a4a29e
meisibley <6671@holbertonstudents.com>
2023-07-20 19:07:06 -0700

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,11 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		ins->n = n;
-		ins->next = temp->next;
-		temp->next->prev = ins;
-		temp->next = ins;
-		ins->prev = NULL;
+		ins = add_dnodeint(h, n);
 		return (ins);
 	}
 	else if (len == idx + 1)

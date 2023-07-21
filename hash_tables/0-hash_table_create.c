a6982dfad560cc13fd6f21579836d91a08db585b
meisibley <6671@holbertonstudents.com>
2023-07-20 21:01:25 -0700

    0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,14 +9,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	hash_node_t *arr;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
-	arr = malloc(sizeof(hash_node_t *) * size);
-	if (arr == NULL)
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 
 	return (ht);

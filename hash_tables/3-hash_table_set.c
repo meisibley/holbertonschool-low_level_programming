3463baa31e3a47ca56958651735d576f441999a1
meisibley <6671@holbertonstudents.com>
2023-07-21 22:37:08 -0700

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node = NULL, *tempi = NULL;
+	hash_node_t *node = NULL, *temp = NULL;
 
 	if (key == NULL || ht == NULL)
 		return (0);
